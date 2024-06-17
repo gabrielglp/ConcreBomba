58f1f78d5b0015dc5a51fd7158f3cc0fae7c8ea4
gabrielglp <gabriel_pg08@hotmail.com>
2024-06-17 10:28:24 -0300

    feat: animação de title e nos smartphones ao entrar na page/ nova lib instalada

--- src/components/SectionHero.tsx
+++ src/components/SectionHero.tsx
@@ -1,3 +1,7 @@
+'use client'
+
+import { useEffect, useRef } from "react";
+
 import Image from "next/image";
 
 import { Container } from "./Container";
@@ -6,12 +10,48 @@ import IconFile from "@/assets/icon-file.svg"
 import IconHand from "@/assets/icon-hand.svg"
 import IconHand02 from "@/assets/icon-hand-02.svg"
 import IconMockup from "@/assets/mockup.svg"
+import gsap from "gsap";
 
 export function SectionHero() {
+
+    const textHeroRef = useRef(null);
+    const mockupLeftRef = useRef(null);
+    const mockupRightRef = useRef(null);
+
+    useEffect(() => {
+        const textHero = textHeroRef.current
+        const mleft = mockupLeftRef.current
+        const mRight = mockupRightRef.current
+
+        gsap.fromTo(mleft, {
+            rotate: 0
+        }, {
+            rotate: -10,
+            duration: 1
+        })
+
+        gsap.fromTo(mRight, {
+            rotate: 0
+        }, {
+            rotate: 10,
+            duration: 1
+        })
+
+        gsap.fromTo(textHero, {
+            opacity: 0,
+            y: 20
+        }, {
+            opacity: 1,
+            y: 0,
+            duration: 1,
+            ease: 'power4.out'
+        })
+    }, []);
+
     return (
         <section className="relative w-full h-sectionHero bg-hero bg-no-repeat bg-light-gray pt-16 bg-top overflow-hidden">
             <Container className="flex flex-col items-center">
-                <div className="w-full max-w-text-hero text-center">
+                <div className="w-full max-w-text-hero text-center opacity-0" ref={textHeroRef}>
                     <h3 className="text-xl font-medium text-gray-actived mb-4">Bombeamento de concreto usinado em toda a Baixada Santista</h3>
                     <h1 className="text-6xl/normal md:text-7xl/normal font-semibold mb-2">Eficiência e qualidade</h1>
                     <div className="flex items-center justify-center gap-10">
@@ -44,12 +84,14 @@ export function SectionHero() {
                     <Image
                         src={IconMockup}
                         alt="Mockup"
-                        className="relative -rotate-12 top-[18px] left-[53px]"
+                        className="relative top-[18px] left-[53px]"
+                        ref={mockupLeftRef}
                     />
                     <Image
                         src={IconMockup}
                         alt="Mockup"
-                        className="relative rotate-12 top-[18px] right-[53px]"
+                        className="relative top-[18px] right-[53px]"
+                        ref={mockupRightRef}
                     />
                 </div>
             </Container>
