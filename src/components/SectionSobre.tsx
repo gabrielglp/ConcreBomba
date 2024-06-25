99d60d1a90d6e4ecd7b0539db9f8420eca8be156
gabrielglp <gabriel_pg08@hotmail.com>
2024-06-24 23:12:57 -0300

    refactor: refactoring to SectionAbout

--- src/components/SectionSobre.tsx
+++ src/components/SectionSobre.tsx
@@ -1,19 +1,19 @@
 import Image from "next/image";
 
-import ImageSobre from "@/assets/image-03.png";
-import Logo from "@/assets/concrete-mixer-construction-machine-illustration-4d79a5.webp";
+import Caminhao from "@/assets/caminhao-de-cimento.png"
 
 export function SectionSobre() {
     return (
         <>
-            <div className="bg-sobre w-full h-sectionHero bg-no-repeat opacity-45 absolute"></div>
-            <div className="z-10 relative flex flex-col items-center top-28 md:top-16">
-                <Image
-                    className="w-32 md:w-44"
-                    src={Logo}
-                    alt="Logo"
-                />
-                <h1 className="text-5xl md:text-7xl text-[#3B3B3B] font-bold pb-48 md:pb-12">MC<span className="text-green-btn">MIX</span></h1>
+            <div className="w-full h-96 bg-black">
+                <div className="flex flex-col items-center">
+                    <Image
+                        src={Caminhao}
+                        alt="caminhao-de-cimento"
+                        className="w-28 mt-28"
+                    />
+                    <p className="font-bold text-white text-5xl">MC<span className="text-green-btn">MIX</span></p>
+                </div>
             </div>
         </>
     )
