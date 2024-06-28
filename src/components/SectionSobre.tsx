14ff3ac83a56bc19fccf60a14f754a22e466928e
Gabrielneves05 <gneves17@outlook.com>
2024-06-28 19:04:11 -0300

    stye: Implementation of responsiveness / adjustments for larger screens

--- src/components/SectionSobre.tsx
+++ src/components/SectionSobre.tsx
@@ -15,6 +15,26 @@ export function SectionSobre() {
                     <p className="font-bold text-white text-5xl">MC<span className="text-green-btn">MIX</span></p>
                 </div>
             </div>
+
+            <div className="flex flex-col md:flex-row items-center justify-between w-full px-10 md:px-20 gap-10 md:gap-64 md:h-80 h-full py-10">
+                <div className="flex flex-col text-end items-end">
+                    <h1 className="font-bold text-6xl mb-1">QUEM</h1>
+                    <h2 className="font-semibold text-2xl mb-4">SOMOS?</h2>
+                    <p className="text-xs md:pl-10 text-gray-actived">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis odio debitis eligendi, possimus, suscipit perferendis velit placeat exercitationem error itaque provident. Aliquam ipsa non debitis deserunt error iure ut neque!</p>
+                </div>
+
+                <div className="flex flex-col text-end items-end">
+                    <h1 className="font-bold text-6xl mb-1">O QUE</h1>
+                    <h2 className="font-semibold text-2xl mb-4">FAZEMOS?</h2>
+                    <p className="text-xs md:pl-10 text-gray-actived">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis odio debitis eligendi, possimus, suscipit perferendis velit placeat exercitationem error itaque provident. Aliquam ipsa non debitis deserunt error iure ut neque!</p>
+                </div>
+
+                <div className="flex flex-col text-end items-end">
+                    <h1 className="font-bold text-6xl mb-1">COMO</h1>
+                    <h2 className="font-semibold text-2xl mb-4">CONTATAR?</h2>
+                    <p className="text-xs md:pl-10 text-gray-actived">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis odio debitis eligendi, possimus, suscipit perferendis velit placeat exercitationem error itaque provident. Aliquam ipsa non debitis deserunt error iure ut neque!</p>
+                </div>
+            </div>
         </>
     )
 }
